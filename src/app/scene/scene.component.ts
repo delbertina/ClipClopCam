0b058541565df5f443af99734fa429f6998f1013
delbertina <6349928+delbertina@users.noreply.github.com>
2023-02-22 21:54:59 -0600

    update webcam feed to be clean and include static crop grid

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -7,10 +7,11 @@ import { WebGLService } from "./services/web-gl.service";
   styleUrls: ['./scene.component.scss']
 })
 export class SceneComponent implements OnInit, AfterViewInit {
-  	
-  @ViewChild('tempImgCanvas') private tempImgCanvas: ElementRef;
+
   @ViewChild('sceneCanvas') private sceneCanvas: ElementRef;
   @ViewChild('sceneVideo') private video: ElementRef;
+  width = 720;
+  height = 405;
 
   public captures: Array<any>;
 
@@ -30,16 +31,55 @@ export class SceneComponent implements OnInit, AfterViewInit {
       navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
           this.video.nativeElement.srcObject = stream;
           this.video.nativeElement.play();
+          this.timerCallback();
       });
-  }
+    }
   }
 
-  capture() {
-    this.video.nativeElement.pause();
+  timerCallback() {
+    if (this.video.nativeElement.paused || this.video.nativeElement.ended) {
+      return;
+    }
+    this.computeFrame();
     setTimeout(() => {
-      this.video.nativeElement.play();
-    }, 1000);
-    this.tempImgCanvas.nativeElement.getContext("2d").drawImage(this.video.nativeElement, 0, 0, 720, 405);
-    this.captures.push(this.tempImgCanvas.nativeElement.toDataURL("image/png"));
-  }
+      this.timerCallback();
+    }, 0);
+  };
+
+  computeFrame() {
+    const context = this.sceneCanvas.nativeElement.getContext("2d");
+    context.drawImage(this.video.nativeElement, 0, 0, this.width, this.height);
+    const widthFifths = [
+      0, this.width/5,
+      (this.width/5)*2,
+      (this.width/5)*3,
+      (this.width/5)*4,
+      this.width
+    ];
+    const heightFifths = [
+      0, this.height/5,
+      (this.height/5)*2,
+      (this.height/5)*3,
+      (this.height/5)*4,
+      this.height/5
+    ];
+
+    context.beginPath();
+    context.rect(widthFifths[1], heightFifths[1], this.width*0.6, this.height*0.6);
+    context.strokeStyle = "grey";
+    context.lineWidth = 5;
+    context.stroke();
+
+    context.beginPath();
+    context.rect(widthFifths[2], heightFifths[1], this.width*0.2, this.height*0.6);
+    context.strokeStyle = "light grey";
+    context.lineWidth = 1;
+    context.stroke();
+
+    context.beginPath();
+    context.rect(widthFifths[1], heightFifths[2], this.width*0.6, this.height*0.2);
+    context.strokeStyle = "light grey";
+    context.lineWidth = 1;
+    context.stroke();
+  };
 }
