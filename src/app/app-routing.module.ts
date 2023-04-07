011e719f8fa143f809c8eacd8cb9a332e7aa4f5f
delbertina <6349928+delbertina@users.noreply.github.com>
2023-04-06 21:13:40 -0500

    Start v3, make the display stage centered on the frame

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from "@angular/router";
 import { SceneComponent } from "./scene/scene.component";
 import { SceneV1Component } from './scene/v1/scene.component';
+import { SceneV2Component } from './scene/v2/scene.component';
 const routes: Routes = [
   { path: "", component: SceneComponent },
+  { path: "v2", component: SceneV2Component },
   { path: "v1", component: SceneV1Component }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { SceneComponent } from './scene/scene.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SceneV1Component } from './scene/v1/scene.component';
+import { SceneV2Component } from './scene/v2/scene.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SceneComponent,
-    SceneV1Component
+    SceneV1Component,
+    SceneV2Component
   ],
   imports: [
     BrowserModule,
