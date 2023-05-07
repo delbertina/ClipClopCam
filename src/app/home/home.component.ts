46e487dad6be26e4d8761e0fcebaa35551adc731
delbertina <6349928+delbertina@users.noreply.github.com>
2023-05-07 14:47:29 -0500

    Build for github and fix error

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'home-component',
   templateUrl: './home.component.html',
-  imports: [NgbTooltipModule],
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {}
